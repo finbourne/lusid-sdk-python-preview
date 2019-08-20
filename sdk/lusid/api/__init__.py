58762adb9e52006faec7d2d8907c7cdc0ed8d8ad
Concourse <concourse@finbourne.com>
2019-08-20 08:03:12 +0000

    Update SDK 0.10.641

--- sdk/lusid/api/__init__.py
+++ sdk/lusid/api/__init__.py
@@ -23,5 +23,6 @@ from lusid.api.results_api import ResultsApi
 from lusid.api.schemas_api import SchemasApi
 from lusid.api.scopes_api import ScopesApi
 from lusid.api.search_api import SearchApi
+from lusid.api.structured_market_data_api import StructuredMarketDataApi
 from lusid.api.system_configuration_api import SystemConfigurationApi
 from lusid.api.transaction_portfolios_api import TransactionPortfoliosApi
