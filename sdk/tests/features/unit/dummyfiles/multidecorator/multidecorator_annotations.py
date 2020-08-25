24424acbe42949ec59c9b7ce288113a5889d89d1
slacki123 <sebastian.lacki@finbourne.com>
2020-08-25 15:34:43 +0100

    SE-301 for merge with develop

--- sdk/tests/features/unit/dummyfiles/multidecorator/multidecorator_annotations.py
+++ sdk/tests/features/unit/dummyfiles/multidecorator/multidecorator_annotations.py
@@ -1,15 +1,24 @@
 import unittest
 
+from parameterized import parameterized
+
 from features.lusid_feature import lusid_feature
 
 
 class Valid2Tests(unittest.TestCase):
 
-    @lusid_feature("F4")
+    @lusid_feature("F1")
     def test_dummy_method_1(self):
         pass  # Empty for testing purposes
 
-    @lusid_feature("F3")
+    @lusid_feature("F2")
+    @classmethod
+    @parameterized.expand(
+        [
+            ("test1", 1),
+            ("test2", 2)
+        ]
+    )
     def test_dummy_method_2(self):
         pass  # Empty for testing purposes
 
