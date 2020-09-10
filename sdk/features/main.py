637e65d8db44a59fe02861d44ef22986ac18024d
slacki123 <sebastian.lacki@finbourne.com>
2020-09-10 14:08:48 +0100

    SE-301 fixing a feature runner bug with passing params, filepath can be absolute

--- sdk/features/main.py
+++ sdk/features/main.py
@@ -12,25 +12,22 @@ def main(argv):
     ap.add_argument('-p', '--package', help='package name from the root folder, not including the root folder. Eg if '
                                             'root folder is "sdk", then package name should be "tests.tutorials", '
                                             'not "sdk.tests.tutorials"')
-    ap.add_argument('-f', '--filename', help='Name of the features file to be created. By default "features.txt" and '
-                                             'will be created in the root sdk folder. Path from the sdk root folder '
-                                             'can be specified in this format '
-                                             '<some-folder>/<another-folder>/filename.txt')
+    ap.add_argument('-f', '--filepath', help='Path and name of the features file to be created. By default '
+                                             '"features.txt" which will be created in the root sdk folder.')
 
     args = vars(ap.parse_args())
     package_name = args["package"]
-    filename = args["filename"]
+    filepath = args["filepath"]
 
     if package_name is None:
-        package = "tests.tutorials"
+        package_name = "tests.tutorials"
 
-    if filename is None:
-        filename = "features.txt"
+    if filepath is None:
+        filepath = os.path.join(get_project_root(), 'features.txt')
 
-    filepath_from_root = os.path.join(get_project_root(), filename)
-
-    feature_list = extract_all_features_from_package(package)
-    write_features_to_file(feature_list, filepath_from_root)
+    feature_list = extract_all_features_from_package(package_name)
+    write_features_to_file(feature_list, filepath)
+    print("File written to: " + filepath)
 
 
 if __name__ == "__main__":
