5abfce71f649d947f23b8242482e29bef5c3a0bc
slacki123 <sebastian.lacki@finbourne.com>
2020-09-14 18:08:40 +0100

    SE-301 added lusidfeature to list of requirements

--- sdk/features/main.py
+++ sdk/features/main.py
@@ -1,10 +1,10 @@
 import sys
 
-from lusidfeatures.entrypoint import get_features_file
+from lusidfeature.entrypoint import extract_features_to_file
 
 
 def main(argv):
-    get_features_file(argv)
+    extract_features_to_file(sys.argv)
 
 if __name__ == "__main__":
     main(sys.argv)
