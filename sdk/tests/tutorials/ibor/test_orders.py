aab3153583feb9ac586568f828f02eeff434f2dd
OmarOsman <omar.osman@finbourne.com>
2021-03-29 14:23:28 +0100

    Test for UpsertSimpleOrder. Add UpsertSimpleOrder tutorial

--- sdk/tests/tutorials/ibor/test_orders.py
+++ sdk/tests/tutorials/ibor/test_orders.py
@@ -2,7 +2,11 @@ import unittest
 import uuid
 import lusid
 import lusid.models as models
-from lusid import *
+from lusid import OrderRequest
+from lusid import OrderSetRequest
+from lusid import PerpetualProperty
+from lusid import PropertyValue
+from lusid import ResourceId
 from utilities import InstrumentLoader
 from utilities import TestDataUtilities
 
@@ -17,35 +21,30 @@ class Orders(unittest.TestCase):
         instrument_loader = InstrumentLoader(cls.instruments_api)
         cls.instrument_ids = instrument_loader.load_instruments()
 
-    def get_guid(self):
-        # creates random alphanumeric code
-        return str(uuid.uuid4())[:12]
-
-    @lusid_feature("F4")
+ #   @lusid_feature("F4")
     def test_upsert_simple_order(self):
-        """Test makes a request for a single order."""
+        """Makes a request for a single order."""
 
-        test_scope = "Orders-SimpleUpsert-TestScope"
+        orders_scope = "Orders-SimpleUpsert-TestScope"
         # Create unique order id
-        order = self.get_guid()
+        order = str(uuid.uuid4())
         # Create ResourceId for order
 
         # Construct order arguments
         instrument_identifiers = {TestDataUtilities.lusid_luid_identifier: self.instrument_ids[0]}
-        order_id = models.ResourceId(test_scope, order)
-        portfolio_id = ResourceId(test_scope, "OrdersTestPortfolio")
-        # The key:This takes the format {domain}/{scope}/{code}, value: Lusid property value'
-        properties = {f"Order/{test_scope}/TIF": PerpetualProperty(f"Order/{test_scope}/TIF", PropertyValue("GTC")),
-                      f"Order/{test_scope}/OrderBook":
-                          PerpetualProperty(f"Order/{test_scope}/OrderBook", PropertyValue("UK Test Orders")),
-                      f"Order/{test_scope}/PortfolioManager":
-                          PerpetualProperty(f"Order/{test_scope}/PortfolioManager", PropertyValue("F Bar")),
-                      f"Order/{test_scope}/Account":
-                          PerpetualProperty(f"Order/{test_scope}/Account", PropertyValue("J Wilson")),
-                      f"Order/{test_scope}/Strategy":
-                          PerpetualProperty(f"Order/{test_scope}/Strategy", PropertyValue("RiskArb"))}
+        order_id = models.ResourceId(orders_scope, order)
+        portfolio_id = ResourceId(orders_scope, "OrdersTestPortfolio")
+        properties = {f"Order/{orders_scope}/TIF": PerpetualProperty(f"Order/{orders_scope}/TIF", PropertyValue("GTC")),
+                      f"Order/{orders_scope}/OrderBook":
+                          PerpetualProperty(f"Order/{orders_scope}/OrderBook", PropertyValue("UK Test Orders")),
+                      f"Order/{orders_scope}/PortfolioManager":
+                          PerpetualProperty(f"Order/{orders_scope}/PortfolioManager", PropertyValue("F Bar")),
+                      f"Order/{orders_scope}/Account":
+                          PerpetualProperty(f"Order/{orders_scope}/Account", PropertyValue("J Wilson")),
+                      f"Order/{orders_scope}/Strategy":
+                          PerpetualProperty(f"Order/{orders_scope}/Strategy", PropertyValue("RiskArb"))}
 
-        order_book_id = ResourceId(test_scope, "OrdersTestBook")
+        order_book_id = ResourceId(orders_scope, "OrdersTestBook")
         quantity = 100
         # Construct request
         order_request = OrderRequest(properties=properties,
@@ -58,12 +57,8 @@ class Orders(unittest.TestCase):
 
         upsert_result = self.orders_api.upsert_orders(order_set_request=order_req)
 
-        assert len(upsert_result.values) == 1
-        assert upsert_result.values[0].to_dict()['id']['code'] == order
-        assert upsert_result.values[0].to_dict()['lusid_instrument_id'] == self.instrument_ids[0]
-        assert upsert_result.values[0].to_dict()['quantity'] == 100
-        assert upsert_result.values[0].to_dict()['properties'][f"Order/{test_scope}/TIF"]['key'] == f"Order/{test_scope}/TIF"
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(len(upsert_result.values),1)
+        self.assertEqual(upsert_result.values[0].to_dict()['id']['code'], order)
+        self.assertEqual(upsert_result.values[0].to_dict()['lusid_instrument_id'], self.instrument_ids[0])
+        self.assertEqual(upsert_result.values[0].to_dict()['quantity'], 100)
+        self.assertEqual(upsert_result.values[0].to_dict()['properties'][f"Order/{orders_scope}/TIF"]['key'] , f"Order/{orders_scope}/TIF")
