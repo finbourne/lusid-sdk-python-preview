f1467932b894f5b93d02af661ae286d4be4e1e9f
Jack Yaffe <44579282+jyaffe@users.noreply.github.com>
2021-04-06 08:50:39 +0100

    SE-1065: Add ApiClientFactory to module imports (#94)

--- sdk/lusid/__init__.py
+++ sdk/lusid/__init__.py
@@ -447,6 +447,7 @@ from lusid.models.weighted_instruments import WeightedInstruments
 
 # import utilities into sdk package
 from lusid.utilities.api_client_builder import ApiClientBuilder
+from lusid.utilities.api_client_factory import ApiClientFactory
 from lusid.utilities.api_configuration import ApiConfiguration
 from lusid.utilities.api_configuration_loader import ApiConfigurationLoader
 from lusid.utilities.refreshing_token import RefreshingToken
