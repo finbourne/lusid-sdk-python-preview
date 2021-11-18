4d0776202ce2e969439a67b139f100fdc8c59538
Stephen LM <20342969+slemasne@users.noreply.github.com>
2021-11-18 19:41:06 +0000

    Revert "SE-1381: add pat support"
    This reverts commit 660e9f56e7668eb1923edad755154d5760855150.

--- sdk/lusid/utilities/api_client_builder.py
+++ sdk/lusid/utilities/api_client_builder.py
@@ -36,8 +36,7 @@ class ApiClientBuilder:
                 f"variables")
 
     @classmethod
-    def resolve_api_token(cls, token=None, api_secrets_filename=None, configuration=None,
-                          id_provider_response_handler=None):
+    def resolve_api_token(cls, token, configuration, id_provider_response_handler):
 
         """
         Description:
@@ -52,10 +51,9 @@ class ApiClientBuilder:
         PAT are used in preference to supplied credentials.
         secrets files are used in preference to environment variables.
 
-        :param str token: The pre-populated access token to use instead of asking Okta for a token
-        :param str api_secrets_filename: The full path to the JSON file containing the API credentials and optional proxy details
-        :param ApiConfiguration configuration: configuration object containing secrets loaded from env vars
-        :param typing.callable id_provider_response_handler: An optional function to handle the Okta response
+        :param ApiConfiguration configuration:
+        :param str token:
+        :param ApiConfiguration configuration:
 
         :return: str api_token
         """
@@ -114,7 +112,7 @@ class ApiClientBuilder:
                 if value is not None:
                     setattr(configuration, key, value)
 
-        api_token = cls.resolve_api_token(token, api_secrets_filename, configuration, id_provider_response_handler)
+        api_token = cls.resolve_api_token(token, configuration, id_provider_response_handler)
 
         # Initialise the API client using the token so that it can be included in all future requests
         config = Configuration(tcp_keep_alive=tcp_keep_alive)
