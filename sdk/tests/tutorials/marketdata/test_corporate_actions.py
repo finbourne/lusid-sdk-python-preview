4b0748c8da6e6d612b65e8490c6a712c8c741f65
Rizwan Saeed <rizwan.saeed@finbourne.com>
2021-12-14 18:28:59 +0000

    update test ids - corporate actions

--- sdk/tests/tutorials/marketdata/test_corporate_actions.py
+++ sdk/tests/tutorials/marketdata/test_corporate_actions.py
@@ -7,7 +7,10 @@ from datetime import datetime, timedelta
 import lusid
 import lusid.models as models
 from lusidfeature import lusid_feature
+
+from lusid import ApiException
 from lusid.utilities.api_client_builder import ApiClientBuilder
+from utilities import IdGenerator
 from utilities.credentials_source import CredentialsSource
 from utilities.test_data_utilities import TestDataUtilities
 
@@ -23,9 +26,21 @@ class CorporateActions(unittest.TestCase):
         cls.transaction_portfolios_api = lusid.TransactionPortfoliosApi(api_client)
         cls.corporate_actions_sources_api = lusid.CorporateActionSourcesApi(api_client)
 
-    def get_guid(self):
-        # creates random alphanumeric code
-        return str(uuid.uuid4())[:12]
+        cls.id_generator = IdGenerator(scope=TestDataUtilities.tutorials_scope)
+
+    @classmethod
+    def tearDownClass(cls):
+        for item in cls.id_generator.pop_scope_and_codes():
+            entity = item[0]
+            scope = item[1]
+            code = item[2]
+            try:
+                if entity == "ca_source":
+                    cls.corporate_actions_sources_api.delete_corporate_action_source(scope, code)
+                elif entity == "portfolio":
+                    cls.portfolios_api.delete_portfolio(scope, code)
+            except ApiException as ex:
+                print(ex)
 
     @lusid_feature("F12-4")
     def test_name_change_corporate_action(self):
@@ -39,10 +54,8 @@ class CorporateActions(unittest.TestCase):
         instrument_name = "instrument-name"
         instrument_original_figi = "FR0123456789"
         instrument_updated_figi = "FR5555555555"
-        portfolio_code = f"corporate-actions-portfolio-{uuid.uuid4()}"
+
         effective_at_date = datetime(2021, 1, 1, tzinfo=pytz.utc)
-        corporate_action_source_code = "name-change-corporate-actions-source"
-        corporate_action_code = "name-change-corporate-action"
 
         # Create two instruments: an "original" instrument which
         # will be renamed and the instrument it will be renamed to.
@@ -62,11 +75,16 @@ class CorporateActions(unittest.TestCase):
                 ),
             }
         )
+        _, scope, portfolio_code = self.id_generator.generate_scope_and_code(
+            "portfolio",
+            scope=TestDataUtilities.tutorials_scope,
+            code_prefix="corporate-actions-portfolio-"
+        )
 
         try:
             # Create a transaction portfolio to hold the original instrument.
             self.transaction_portfolios_api.create_portfolio(
-                scope=TestDataUtilities.tutorials_scope,
+                scope=scope,
                 create_transaction_portfolio_request=models.CreateTransactionPortfolioRequest(
                     code=portfolio_code,
                     display_name=portfolio_code,
@@ -98,6 +116,11 @@ class CorporateActions(unittest.TestCase):
                 )
             ],
         )
+
+        corporate_action_source_code = "name-change-corporate-actions-source"
+        corporate_action_code = "name-change-corporate-action"
+
+        self.id_generator.add_scope_and_code("ca_source", TestDataUtilities.tutorials_scope, corporate_action_source_code)
 
         # Create a corporate actions source.
         corporate_action_source = models.CreateCorporateActionSourceRequest(
@@ -167,6 +190,13 @@ class CorporateActions(unittest.TestCase):
             ],
         )
 
+        self.id_generator.add_scope_and_code(
+            "corp_action",
+            TestDataUtilities.tutorials_scope,
+            corporate_action_source_code,
+            [corporate_action_code]
+        )
+
         # Create a request to upsert a corporate action with the transition above.
         corporate_action_request = models.UpsertCorporateActionRequest(
             corporate_action_code=corporate_action_code,
@@ -178,7 +208,7 @@ class CorporateActions(unittest.TestCase):
         )
 
         # Make the request through the CorporateActionSourcesApi.
-        upsert_corp_act_response = self.corporate_actions_sources_api.batch_upsert_corporate_actions(
+        self.corporate_actions_sources_api.batch_upsert_corporate_actions(
             scope=TestDataUtilities.tutorials_scope,
             code=corporate_action_source_code,
             upsert_corporate_action_request=[corporate_action_request],
@@ -199,48 +229,17 @@ class CorporateActions(unittest.TestCase):
             instrument_updated_figi,
         )
 
-        # Remove all data that was created for the test.
-        # Delete the two instruments.
-        # self.instruments_api.delete_instrument("Figi", instrument_original_figi)
-        #
-        # self.instruments_api.delete_instrument("Figi", instrument_updated_figi)
-
-        # Delete the portfolio.
-        self.portfolios_api.delete_portfolio(
-            TestDataUtilities.tutorials_scope, portfolio_code
-        )
-
-        try:
-            # Delete the corporate action source.
-            self.corporate_actions_sources_api.delete_corporate_action_source(
-                TestDataUtilities.tutorials_scope, corporate_action_source_code
-            )
-        except lusid.ApiException as e:
-            if json.loads(e.body)["name"] == "CorporateActionSourceDoesNotExist":
-                pass  # ignore if the property definition exists
-
     @lusid_feature("F33")
     def test_list_corporate_action_sources(self):
-        _uuid = self.get_guid()
-        request = models.CreateCorporateActionSourceRequest(
-            scope=TestDataUtilities.tutorials_scope,
-            code=f"test-corp-action-code-{_uuid}",
-            display_name=f"test-corp-action-{_uuid}",
+
+        _, scope, code, = self.id_generator.generate_scope_and_code(
+            "ca_source",
+            TestDataUtilities.tutorials_scope,
+            code_prefix="test-corp-action"
         )
+
+        request = models.CreateCorporateActionSourceRequest(scope=scope, code=code, display_name=code)
+
         self.corporate_actions_sources_api.create_corporate_action_source(request)
         sources = self.corporate_actions_sources_api.list_corporate_action_sources()
         self.assertGreater(len(sources.values), 0)
-
-    @unittest.skip("Not Implemented")
-    def test_list_corporate_actions_for_one_day(self):
-        _uuid = self.get_guid()
-        request = models.CreateCorporateActionSourceRequest(
-            scope=TestDataUtilities.tutorials_scope,
-            code=f"test-corp-action-code-{_uuid}",
-            display_name=f"test-corp-action-{_uuid}",
-        )
-        self.corporate_actions_sources_api.create_corporate_action_source(request)
-        sources = self.corporate_actions_sources_api.get_corporate_actions(
-            scope=f"test-corp-action-code-{_uuid}",
-            code=f"test-corp-action-code-{_uuid}",
-        )
