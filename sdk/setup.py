bc0e25ca25f81466e6f4545ce0b6075b55c24fea
Rizwan Saeed <rizwan.saeed@finbourne.com>
2022-06-03 07:07:50 +0100

    CTECH-1368 update setup.py versions

--- sdk/setup.py
+++ sdk/setup.py
@@ -32,12 +32,11 @@ from setuptools import setup, find_packages  # noqa: H301
 # http://pypi.python.org/pypi/setuptools
 
 REQUIRES = [
-    "urllib3 >= 1.15",
-    "six >= 1.10",
     "certifi >= 14.05.14",
     "python-dateutil >= 2.5.3",
-    "urllib3 >= 1.15.1",
-    "requests >= 2.21.0"
+    "requests >= 2.27.1",
+    "six >= 1.10",
+    "urllib3 >= 1.26.9",
 ]
 
 version = {}
