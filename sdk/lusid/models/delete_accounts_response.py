ebd9f535ff2a2f79dbf3cd2051b7467ce3458350
Concourse <concourse@finbourne.com>
2023-01-20 09:09:14 +0000

    Update SDK 0.11.5159 [rebase]

--- sdk/lusid/models/delete_accounts_response.py
+++ sdk/lusid/models/delete_accounts_response.py
@@ -63,7 +63,7 @@ class DeleteAccountsResponse(object):
         :type version: lusid.Version
         :param account_ids:  The Accounts which have been soft/hard deleted.
         :type account_ids: list[str]
-        :param links:  Collection of links.
+        :param links: 
         :type links: list[lusid.Link]
 
         """  # noqa: E501
@@ -129,7 +129,6 @@ class DeleteAccountsResponse(object):
     def links(self):
         """Gets the links of this DeleteAccountsResponse.  # noqa: E501
 
-        Collection of links.  # noqa: E501
 
         :return: The links of this DeleteAccountsResponse.  # noqa: E501
         :rtype: list[lusid.Link]
@@ -140,7 +139,6 @@ class DeleteAccountsResponse(object):
     def links(self, links):
         """Sets the links of this DeleteAccountsResponse.
 
-        Collection of links.  # noqa: E501
 
         :param links: The links of this DeleteAccountsResponse.  # noqa: E501
         :type links: list[lusid.Link]
